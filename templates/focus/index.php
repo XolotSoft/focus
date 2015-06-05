fccfa39c4e5b9780ff4f28bf5e8a223f22897eb3
kobe <luisalbertoruiz.geek@gmail.com>
2015-06-05 14:06:49 -0500

    navbar a plantilla focus

--- templates/focus/index.php
+++ templates/focus/index.php
@@ -5,7 +5,6 @@ include_once JPATH_THEMES.'/'.$this->template.'/logic.php';
 ?><!doctype html>
 
 <html lang="<?php echo $this->language; ?>">
-
 <head>
 	<jdoc:include type="head" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
@@ -19,16 +18,25 @@ include_once JPATH_THEMES.'/'.$this->template.'/logic.php';
 	<script type="text/javascript" src="<?php echo $tpath; ?>/js/respond.min.js"></script>
 	<![endif]-->
 </head>
-  
 <body class="<?php echo (($menu->getActive() == $menu->getDefault()) ? ('front') : ('site')).' '.$active->alias.' '.$pageclass; ?>" role="document">
-
-	<?php require_once 'html/bootstrap.test.php'; // test only - delete this line ?>
-
-	<!-- 
-		YOUR CODE HERE
-	-->
-
-	<jdoc:include type="modules" name="debug" />
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Focus On Language</a>
+		</div>
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<jdoc:include type="modules" name="navbar" />
+		</div>
+	</nav>
+	<div class="container">
+		<jdoc:include type="modules" name="contenido" />
+		<jdoc:include type="modules" name="debug" />
+	</div>
 </body>
-
 </html>
