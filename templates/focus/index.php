b0331a3796bc69dd5500b7ac6e758a465caf336a
kobe <luisalbertoruiz.geek@gmail.com>
2015-06-15 13:30:43 -0500

    cambio de color y bordes a navbar

--- templates/focus/index.php
+++ templates/focus/index.php
@@ -19,23 +19,35 @@ include_once JPATH_THEMES.'/'.$this->template.'/logic.php';
 	<![endif]-->
 </head>
 <body class="<?php echo (($menu->getActive() == $menu->getDefault()) ? ('front') : ('site')).' '.$active->alias.' '.$pageclass; ?>" role="document">
-	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">Focus On Language</a>
-		</div>
-		<div class="collapse navbar-collapse navbar-ex1-collapse">
-			<jdoc:include type="modules" name="navbar" />
-		</div>
-	</nav>
 	<div class="container">
+	<div class="row">
+		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+			<div class="page-header">
+			  <img src="images/logo.png" class="img-responsive" alt="Image">
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<nav class="navbar navbar-default" role="navigation">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Focus On Language</a>
+			</div>
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<jdoc:include type="modules" name="navbar" />
+			</div>
+		</nav>
+	</div>
 		<jdoc:include type="modules" name="contenido" />
+					<jdoc:include type="message" />
+					<jdoc:include type="component" />
+		<jdoc:include type="modules" name="footer" />
 		<jdoc:include type="modules" name="debug" />
 	</div>
 </body>
